24705ff4d0ee8cb1900c9369226612c0c62bb72b
Weine Oliveira <weine.santos.de.oliveira@ibm.com>
2019-12-15 13:28:59 -0300

    adding cors

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const routes = require('./routes');
+const cors = require('cors');
 
 
 app.use(bodyParser.json());
+app.use(cors);
 app.use(routes);
 
 module.exports = app;
